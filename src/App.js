4939d5e9b944366a79cdb591288affd5761ebaad
Jacob Castiglioni <jacobcastiglioni@Jacobs-MacBook-Pro.local>
2021-02-03 19:10:12 -0500

    added game component, refactored files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
 import React, {useState, useEffect} from 'react'
-import Tile from './components/Tile'
-import Popup from './components/Popup'
+
 import Edit from './components/Edit'
 import './styles/App.css';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch, Route,
+} from 'react-router-dom';
+import Game from './components/Game'
+import {Link} from 'react-router-dom'
 
 const App = () => {
-  const [tiles, setTiles] = useState([])
-  const [popup, setPopup] = useState(false)
-  const [question, setQuestion] = useState('')
-  const [audio, setAudio] = useState(false)
-  
-  
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then (response => {
-        setTiles(response.data)
-      })
-  }, [])
 
-  const setActiveQuestion = (tile) => {
-    setQuestion(tile.text.toUpperCase())
-    setAudio(tile.audio)
-    tile.value = ''
-    togglePopup()
-  }
 
-  const togglePopup = () => {
-    setPopup(!popup)
+  const padding = {
+    padding: '5px'
   }
 
   return (
     <div className="App">
-      <button className="col">MUSIC</button>
-          {tiles.map(obj => <Tile func={() => setActiveQuestion(obj)}key={obj.text} tile={obj.value} />)}
-            <Popup func={() => togglePopup()} trigger={popup} text={question} audio={audio} />
-          <br/><br/>
-        <Edit />
+      <div>
+
+        <Router>
+        <Link style={padding} to="/">HOME</Link>
+        
+          <Link style={padding} to="/edit">EDIT</Link>
+          
+
+          <Switch>
+            <Route path='/edit' component={Edit} />
+            <Route path='/' component={Game} />
+          </Switch>
+
+        </Router>
+      </div>
     </div>    
   )
 }
